c82b465060f6a896523c8d563080a405876255b9
Maxeem <jmax.posh@gmail.com>
2019-07-12 12:11:47 +0300

    28 answer

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,24 @@
 package iii_conventions
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
+import java.util.*
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    override operator fun compareTo(other: MyDate) = when {
-        year != other.year -> year - other.year
-        month != other.month -> month - other.month
-        else -> dayOfMonth.compareTo(other.dayOfMonth)
-    }
+class DateRange(override val start: MyDate, override val endInclusive: MyDate)
+        : ClosedRange<MyDate>, Iterable<MyDate>, Iterator<MyDate> {
+
+    private var next = start
+
+    override operator fun hasNext() = next <= endInclusive
+    override operator fun next() = next.let { next = next.nextDay(); it }
+
+    override operator fun iterator() = this
+}
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+
+    internal val c = Calendar.getInstance().apply { clear(); set(year, month, dayOfMonth) }
+
+    override operator fun compareTo(other: MyDate) = c.compareTo(other.c)
+
 }
 
 operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
--- src/iii_conventions/MyDateUtil.kt
+++ src/iii_conventions/MyDateUtil.kt
@@ -1,16 +1,15 @@
 package iii_conventions
 
-import iii_conventions.TimeInterval.DAY
+import iii_conventions.TimeInterval.*
 import java.util.*
 
 fun MyDate.nextDay() = addTimeIntervals(DAY, 1)
 
-fun MyDate.addTimeIntervals(timeInterval: TimeInterval, number: Int) = Calendar.getInstance().run {
-    set(year, month, dayOfMonth)
+fun MyDate.addTimeIntervals(timeInterval: TimeInterval, number: Int) = c.clone().let { it as Calendar }.run {
     add(when (timeInterval) {
-        TimeInterval.DAY -> Calendar.DAY_OF_MONTH
-        TimeInterval.WEEK -> Calendar.WEEK_OF_MONTH
-        TimeInterval.YEAR -> Calendar.YEAR
+        DAY -> Calendar.DAY_OF_MONTH
+        WEEK -> Calendar.WEEK_OF_MONTH
+        YEAR -> Calendar.YEAR
     }, number)
     MyDate(get(Calendar.YEAR), get(Calendar.MONTH), get(Calendar.DATE))
 }
